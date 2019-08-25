5130f135783bc73af782ab06d666720e3bd6ae26
Gowtham Babu Tummala <gowtham.t4@gmail.com>
2019-08-25 21:15:40 +0200

    Update RockPaperScissorsImpl.java
    Added Comments

--- src/main/java/com/getaway/rockpaperscissors/service/RockPaperScissorsImpl.java
+++ src/main/java/com/getaway/rockpaperscissors/service/RockPaperScissorsImpl.java
@@ -3,7 +3,11 @@ package com.getaway.rockpaperscissors.service;
 import com.getaway.rockpaperscissors.entities.HandGame;
 
 public class RockPaperScissorsImpl {
-
+	
+	/*
+	 * As Player1 constantly forms the ROCK shape, only Player2 shape is considered
+	 * to the below function
+	 */
 	public String findResult(int shapeValue) {
 		// TODO Auto-generated method stub
 		if(shapeValue == HandGame.ROCK.getShapeValue()) {
@@ -16,7 +20,8 @@ public class RockPaperScissorsImpl {
 	}
 	
 	/*
-	 * Player1 Result is bound to Player2 result
+	 * Player1 Result is bound to Player2 result.
+	 * Player1 result is calculated using Player2 result from the below function
 	 */
 	public String findPlayer1Result(String player2Result) {
 		// TODO Auto-generated method stub
